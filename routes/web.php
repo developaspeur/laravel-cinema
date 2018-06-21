87395eb9bd527609602e81e01a2c341df55b16bc
Jonathan <contact@jonathan-fortune.fr>
2018-06-21 14:16:35 +0200

    début définition des routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
+// Accueil
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/', function () {
     return view('welcome');
 });
